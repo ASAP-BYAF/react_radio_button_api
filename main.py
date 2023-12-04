8314ed4574ac66208f616ee41248daa7c5d5fd0e
teppei <teppei0830.4869@gmail.com>
2023-12-04 20:55:53 +0900

    main.py の hostname と port を .env で管理するようにしました。

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import uvicorn
-
+from dotenv import load_dotenv
+import os
+from pathlib import Path
 
 if __name__ == "__main__":
-    uvicorn.run("api.main:app", host="0.0.0.0", port=8008, reload=True)
+    BASE_DIR = Path(__file__).resolve().parent
+    
+    # 環境変数を .env から読み込む。
+    dotenv_path = os.path.join(BASE_DIR, '.env')
+    load_dotenv(dotenv_path)
+    HOST = os.environ.get("HOST")
+    PORT = int(os.environ.get("PORT"))
+    uvicorn.run("api.main:app", host=HOST, port=PORT, reload=True)
