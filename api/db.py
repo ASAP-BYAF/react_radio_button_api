d521652d0ec6512d2c3af48ad7598ee95f9934a4
teppei <teppei0830.4869@gmail.com>
2023-12-07 21:15:02 +0900

    api/db.py を修正しました。
    HOST 名などを .env で管理するようにしました。
    本番環境と開発環境で使い分けをしやすくするためです。

--- api/db.py
+++ api/db.py
@@ -9,10 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # 環境変数を .env から読み込む。
 dotenv_path = os.path.join(BASE_DIR, '.env')
 load_dotenv(dotenv_path)
-DB_USER = os.environ.get("DB_USER", "root")
+DB_DRIVER = os.environ.get("DB_DRIVER", "")
+DB_USER = os.environ.get("DB_USER", "")
 DB_PASS = os.environ.get("DB_PASS", "")
+DB_HOST = os.environ.get("DB_HOST", "localhost")
+DB_NAME = os.environ.get("DB_NAME", "")
 ASYNC_DB_URL = (
-f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@localhost:5432/test_api_db"
+f"{DB_DRIVER}://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
 )
 
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
